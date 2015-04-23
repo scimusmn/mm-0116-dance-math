dff0605d18e70c860f88aff1ee70036cdc84c971
Trygve <tnordberg@smm.org>
2015-04-23 16:05:04 -0500

    Updated to new shape sounds

--- src/DMLayout.cpp
+++ src/DMLayout.cpp
@@ -48,6 +48,7 @@ void DMLayout::setupViews(){
     button("btnRecordNow", 1490, 180, "start_dance_1");
     button("btnRecordNow", 1490, 421, "start_dance_0.5");
 //    button("btnRecordNow", 1490, 662, "start_dance_2");
+    button("invisiButton", 1490, 662, "start_dance_2");
     button("btnStartOver", 1600, 915, "start_over");
     button("btnTogglePlayback", 1200, 1000, "toggle_playback");
     saveView(VIEW_PLAYBACK);
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -22,10 +22,10 @@ void ofApp::setup(){
     jukebox.addSong("waltz", "sounds/waltz.wav", 14567, 7107, 943);
     jukebox.addSong("tango", "sounds/tango.wav", 10236, 3783, 466);
      */
-    jukebox.addSong("jazz", "sounds/clicks.wav", 14567, 7107, 943);
-    jukebox.addSong("clicks", "sounds/clicks.wav", 14567, 7107, 943);
-    jukebox.addSong("waltz", "sounds/ClickTrackGo.wav", 14567, 7107, 943);
-    jukebox.addSong("tango", "sounds/ClickTrackGo.wav", 14567, 7107, 943);
+    jukebox.addSong("jazz", "sounds/circle.wav", 14567, 7107, 943);
+    jukebox.addSong("clicks", "sounds/triangle.wav", 14567, 7107, 943);
+    jukebox.addSong("waltz", "sounds/square.wav", 14567, 7107, 943);
+    jukebox.addSong("tango", "sounds/clicks.wav", 14567, 7107, 943);
 
     //Load/Setup UI
     layout.setupViews();
